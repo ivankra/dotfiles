1ab681982afd51e96ba18de98c636173832e1ec4
ivank <ivank@ws>
2010-12-06 20:51:38 +0300

    rm vim.patch: functionality appeared in vim 7.3.074

--- ubuntu/vim.sh
+++ ubuntu/vim.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-# Compiles cutting-edge vim in which "set clipboard=unnamed" yanks to plus register on X11
+# Compiles cutting-edge vim
 set -e -x
-#sudo apt-get build-dep vim-gnome
+sudo apt-get build-dep vim-gnome
 cd /tmp
 rm -rf /tmp/vim-hg
 hg clone https://vim.googlecode.com/hg/ vim-hg
 cd vim-hg
-hg import --no-commit ~/git/configs/ubuntu/vim.patch
+#hg import --no-commit ~/git/configs/ubuntu/vim.patch
 make distclean
 ./configure --prefix=/usr/local --with-features=huge --with-x --enable-gui=gnome2 --enable-cscope --enable-multibyte --enable-pythoninterp --disable-nls
 make
