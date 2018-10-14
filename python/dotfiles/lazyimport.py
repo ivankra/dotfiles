e32d257ffd7d40eb2ee8a10072b174ed6f9b7aa8
ivank <ivank@ws>
2018-10-14 22:56:24 +0200

    Fixes

--- python/dotfiles/lazyimport.py
+++ python/dotfiles/lazyimport.py
@@ -4,21 +4,21 @@
 # Copyright 2015-2016, the xonsh developers. All rights reserved.
 # Copyright (c) 2016, xonsh
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
-# 
+#
 # * Redistributions in binary form must reproduce the above copyright notice,
 #   this list of conditions and the following disclaimer in the documentation
 #   and/or other materials provided with the distribution.
-# 
+#
 # * Neither the name of lazyasd nor the names of its
 #   contributors may be used to endorse or promote products derived from
 #   this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -151,11 +151,14 @@ def lazyimport(scope, *mod_list, **mod_kw):
         mod_kw[key] = key
 
     for key, mod in mod_kw.items():
-        if key in scope:
-            continue
-        elif callable(mod):
-            scope[key] = LazyObject(mod, scope, key)
-        elif mod in sys.modules:
-            scope[key] = sys.modules[mod]
-        elif importlib.util.find_spec(mod) is not None:
-            scope[key] = LazyObject(lambda m=mod: importlib.import_module(m), scope, key)
+        try:
+            if key in scope:
+                continue
+            elif callable(mod):
+                scope[key] = LazyObject(mod, scope, key)
+            elif mod in sys.modules:
+                scope[key] = sys.modules[mod]
+            elif importlib.util.find_spec(mod) is not None:
+                scope[key] = LazyObject(lambda m=mod: importlib.import_module(m), scope, key)
+        except:
+            pass
