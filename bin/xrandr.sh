b1b9dff0a230a3c8dab98ee5eb5381973563eff1
ivank <ivank@ws>
2020-04-07 10:55:29 +0200

    xrand.sh: accept float ratio

--- bin/xrandr.sh
+++ bin/xrandr.sh
@@ -6,10 +6,22 @@ set -e -o pipefail
 
 width="$1"
 if [[ "$width" =~ ^([0-9]+)x([0-9]+)$ ]]; then
+  # xrand.sh <width>x<height> [<refresh>]
   width="${BASH_REMATCH[1]}"
   height="${BASH_REMATCH[2]}"
   refresh_rate="$2"
+elif [[ "$width" =~ 0?\.[0-9]+ ]]; then
+  # xrandr.sh <ratio> [<refresh>]
+  ratio="$1"
+  refresh_rate="$2"
+
+  curr=$(xrandr | sed -ne 's/^Screen .* current \([0-9]\+\) x \([0-9]\+\).*/\1 \2/p' | head -1)
+  curw=$(echo "$curr" | cut -d " " -f 1)
+  curh=$(echo "$curr" | cut -d " " -f 1)
+  width=$(python -c "print(int($curw * $ratio))")
+  height=$(python -c "print(int($curh * $ratio))")
 else
+  # xrandr.sh <width> <height> [<refresh>]
   height="$2"
   refresh_rate="$3"
 fi
