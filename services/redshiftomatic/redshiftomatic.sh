cb7d05593b9994fe51e0f16d2b4226c93c6d2c2c
ivank <ivank@ws>
2020-05-20 16:16:44 +0200

    redshiftomatic fixes

--- services/redshiftomatic/redshiftomatic.sh
+++ services/redshiftomatic/redshiftomatic.sh
@@ -1,30 +1,50 @@
 #!/bin/bash
 # Automatically reapplies redshift settings on any display connectivity change.
-set -eu
+set -e
 
 cmd() {
-  redshift -O 3500K -b 1 -P
+  if ! redshift -O 3500K -b 1 -P; then
+    echo "redshift failed!"
+    sleep 3
+
+    local user=$(whoami)
+    local n=0
+    while ! (loginctl show-user "$user" 2>&1 | grep -q State=active); do
+      ((++n))
+      if [[ $n == 1 ]]; then
+        echo "User logged out"
+      fi
+      sleep 3
+    done
+  fi
 }
 
-cmd_rep() {
-  for attempt in $(seq $1); do
+cmd_wrap() {
+  for attempt in $(seq 20); do
     cmd
     sleep 1
   done
+  touch event
 }
 
-WORKDIR=$(mktemp -d --tmpdir="${XDG_RUNTIME_DIR:-/tmp}" redshiftomaticXXXX)
-cd "$WORKDIR"
+main() {
+  if [[ -z "$DISPLAY" ]]; then
+    export DISPLAY=:0
+  fi
 
-echo "State file: $WORKDIR/event"
-cmd_rep 30
-touch event
+  WORKDIR="${XDG_RUNTIME_DIR:-/tmp}"/redshiftomatic
+  mkdir -p "$WORKDIR"
+  cd "$WORKDIR"
+  echo "$$" >pid
+  cmd_wrap
 
-tail --follow=name --retry /var/log/Xorg.0.log | \
-  stdbuf -oL egrep ': (dis)?connected$' | \
-  while IFS=$'\n' read line; do
-    if [[ "$(date +%s)" != "$(date -r event +%s)" ]]; then
-      cmd_rep 10
-      touch event
-    fi
-  done
+  tail --follow=name --retry /var/log/Xorg.0.log | \
+    stdbuf -oL egrep ': ((dis)?connected$|Setting mode)' | \
+    while IFS=$'\n' read line; do
+      if [[ "$(date +%s)" != "$(date -r event +%s)" ]]; then
+        cmd_wrap
+      fi
+    done
+}
+
+main
